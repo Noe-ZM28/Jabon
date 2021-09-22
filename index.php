6a6d1a9144fd3c485b3662d5c1355b8b008e910c
KrillerGod34 <brinknene@gmail.com>
2021-09-21 23:29:19 -0500

    Se agrego texto al index

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 ?>
 
             <title>Jabones</title>
-            <div>
+            <div class="container">
                 <center><h1>Bienvenida</h1></center>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis aut laborum fugit eveniet tenetur itaque eum, ab quisquam fuga sapiente inventore dolore consequatur? Consectetur quia velit voluptatibus itaque laborum quis ut sint tempora, alias esse maxime repudiandae odio quidem reprehenderit harum cupiditate quos possimus deserunt fugiat nisi et dolor. Aspernatur, expedita earum! Odit ratione optio labore dolor facilis, reiciendis sunt laborum dolorum quod! Inventore debitis pariatur odio culpa fuga doloribus et itaque, sequi ab suscipit animi esse natus similique tempora voluptate nostrum harum excepturi. Voluptatibus perferendis ab natus itaque, vero aut expedita voluptatum odit, possimus sint sunt? Magni, libero esse.</p>
+            </div>
+            <div class="container">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, harum. Sequi optio possimus nihil porro delectus repudiandae rem, doloremque illo.</p>
             </div>
             
 
