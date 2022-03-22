a5e6fcd904c612fbe9935f35391c4d6da63b0888
Noé Zúñiga <brinknene@gmail.com>
2022-03-21 20:44:50 -0600

    pertenece al commit anterior

--- HTML/guardar_usuario.php
+++ HTML/guardar_usuario.php
@@ -1,25 +1,26 @@
 <?php 
 include_once "funciones.php";
 $HEADDER = obtenerVariableDelEntorno("HEADDER");
-include_once $HEADDER;
 
-if ($_POST["usuario"] == "Administrador" || "administrador") { ?>
+
+if ($_POST["usuario"] == "Administrador") { 
+    include_once $HEADDER;?>
     <section class="hero is-info">
-    <div class="hero-body">
-        <div class="container">
-            <h1 class="title">
-                Error
-            </h1>
-            <h2 class="subtitle">
-                Nombre de usuario no permitido, favor de seleccinar uno diferente.
-            </h2>
-            <center><a href="formulario_registro.php" class="button is-danger">Regresar</a></center>
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    Error
+                </h1>
+                <h2 class="subtitle">
+                    El nombre de usuario no esta permitido, favor de seleccinar uno diferente.
+                </h2>
+                <center><a href="formulario_registro.php" class="button is-danger">Regresar</a></center>
+            </div>
         </div>
-    </div>
-</section><?php
+    </section><?php
+    include_once "Includes/Footer.php";
 } else {
-    guardarUsuario($_POST["usuario"], $_POST["apellidos"], $_POST["email"], $_POST["contraseña"]);
+    guardarUsuario($_POST["usuario"], $_POST["apellidos"], $_POST["email"], $_POST["numero"], $_POST["contraseña"]);
     header("Location: formulario_inicio_sesion.php");
 }
 
-include_once "Includes/Footer.php" ?>
